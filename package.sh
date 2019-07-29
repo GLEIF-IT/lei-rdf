0dd183d7d2e7a1a6b42661e05d252a01106b7b94
bryon <bryon@data.world>
2019-07-29 13:02:43 -0500

    updated package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
+mkdir target
 ls -1 ontology/ | grep ttl | cut -d '.' -f 1 | while read ontology ; do python publish.py ontology/$ontology.ttl publishorder/$ontology.txt target/$ontology.ttl ; done
 ls -1 ontology/ | grep ttl | cut -d '.' -f 1 | while read ontology ; do java -jar ~/.m2/repository/es/oeg/widoco/1.4.9/widoco-1.4.9-launcher.jar -ontFile target/$ontology.ttl -outFolder target/$ontology -rewriteAll ; done
 ls -1 ontology/ | grep ttl | cut -d '.' -f 1 | while read ontology ; do mv target/$ontology.ttl target/$ontology/ontology.ttl ; done
 ls -1 ontology/ | grep ttl | cut -d '.' -f 1 | while read ontology ; do sed -E -i ".bak" "/#description|#introduction|#references/d" target/$ontology/index-en.html ; done
+ls -1 ontology/ | grep ttl | cut -d '.' -f 1 | while read ontology ; do sed -E -i ".bak" "s/Ontology Specification Draft/Ontology Specification/g" target/$ontology/index-en.html ; done
 
