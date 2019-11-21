4ba11583c6c78eb26c6dfecc2b4ce01a42e9edc6
rivettp <pete.rivett@federatedknowledge.com>
2019-11-21 05:58:50 -0800

    Updated ELF and RA scripts as per issue #169

--- code/process-elf.sh
+++ code/process-elf.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-# Param is the full filename with no extension
+# Param 1 is the full filename with no extension
 # e.g. 2019-07-19_elf-code-list-publication-version-1.1
+# Param 2 is the dataset on data.world including username e.g. rivettp/gleif-2019-10-31
+# Requires shell variable DATAWORLD_TOKEN for registered user API token
 
+echo Processing ELF file $1 to dataset $2
+echo Fetching file $1 from GLEIF site
 curl -O https://www.gleif.org/content/2-about-lei/7-code-lists/2-iso-20275-entity-legal-forms-code-list/$1.csv
+
+echo Converting to $1.ttl
 python3 elf-to-rdf.py $1.csv $1.ttl
-# FTP $1.rdf to GLEIF
 
 # Upload to GLEIF
-# Uses rivettp's API token
-token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnJpdmV0dHAiLCJpc3MiOiJhZ2VudDpyaXZldHRwOjo3NWZmODA5OS1lYzI2LTQzYTktYjk4Zi1lNTRhYjM5ZTM2MTkiLCJpYXQiOjE1MDcyNDEwMzAsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSwic2FtbCI6e319.ioambjK--OqAGzVJM82qYyxLFSHCW4KYqs1SNQV-g2bRdFUURaMZl4RBq3nGMIorufxdb0XcVHrnJCq3YEIg4A"
+# TBD
 
-curl -H "Authorization: Bearer $token" \
+# Upload to data.world 
+echo uploading $1.ttl to data.world $2/files/EntityLegalFormData.ttl
+
+curl -H "Authorization: Bearer $DATAWORLD_TOKEN" \
   -X PUT -H "Content-Type: application/octet-stream" \
   --data-binary @$1.ttl \
-  https://api.data.world/v0/uploads/rivettp/gleif-2019-10-31/files/EntityLegalFormData.ttl
+  https://api.data.world/v0/uploads/$2/files/EntityLegalFormData.ttl
   
-
+echo
+echo ELF processing complete
--- code/process-ra.sh
+++ code/process-ra.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # Param is the full filename with no extension
 # e.g. 2018-12-12_ra-list_v1.4
+# Param 2 is the dataset on data.world including username e.g. rivettp/gleif-2019-10-31
+# Requires shell variable DATAWORLD_TOKEN for registered user API token
 
+echo Processing RA file $1 to dataset $2
+echo Fetching file $1 from GLEIF site
 curl -O https://www.gleif.org/content/2-about-lei/7-code-lists/1-gleif-registration-authorities-list/$1.csv
+
+echo Converting to $1.ttl
 python3 ra-to-rdf.py $1.csv $1.ttl
-# FTP $1.rdf to GLEIF
 
 # Upload to GLEIF
-# Uses rivettp's API token
-token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnJpdmV0dHAiLCJpc3MiOiJhZ2VudDpyaXZldHRwOjo3NWZmODA5OS1lYzI2LTQzYTktYjk4Zi1lNTRhYjM5ZTM2MTkiLCJpYXQiOjE1MDcyNDEwMzAsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSwic2FtbCI6e319.ioambjK--OqAGzVJM82qYyxLFSHCW4KYqs1SNQV-g2bRdFUURaMZl4RBq3nGMIorufxdb0XcVHrnJCq3YEIg4A"
+# TBD
 
-curl -H "Authorization: Bearer $token" \
+# Uses rivettp's API token
+echo uploading $1.ttl to data.world $2/files/RegistrationAuthorityData.ttl
+
+curl -H "Authorization: Bearer $DATAWORLD_TOKEN" \
   -X PUT -H "Content-Type: application/octet-stream" \
   --data-binary @$1.ttl \
-  https://api.data.world/v0/uploads/rivettp/gleif-2019-10-31/files/RegistrationAuthorityData.ttl
+  https://api.data.world/v0/uploads/$2/files/RegistrationAuthorityData.ttl
   
-
+echo 
+echo RA processing complete
