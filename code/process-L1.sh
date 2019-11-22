59c1b420583cbc7a6c842b5f1fbe246a4be967d0
rivettp <pete.rivett@federatedknowledge.com>
2019-11-22 03:20:31 -0800

    Fix redirect for L1 logging

--- code/process-L1.sh
+++ code/process-L1.sh
@@ -7,6 +7,6 @@ echo Unzipping
 unzip -o $1.xml.zip
 
 echo Applying GLEIF-L1.xsl to $1.xml to produce $1.rdf
-java -cp ~/saxon/saxon9ee.jar net.sf.saxon.Transform -s:$1.xml -xsl:GLEIF-L1.xsl -o:$1.rdf >L1-xsl.log
+java -cp ~/saxon/saxon9ee.jar net.sf.saxon.Transform -s:$1.xml -xsl:GLEIF-L1.xsl -o:$1.rdf 2>L1-xsl.log
   
 echo L1 transformation complete
